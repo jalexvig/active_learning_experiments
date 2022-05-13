b00021f80f917b8e3001d782cc4a4dfb386aae2b
jalexvig <>
2022-05-13 11:20:53 -0400

    Update CGMM

--- active_learning/multi_distr_output.py
+++ active_learning/multi_distr_output.py
@@ -10,50 +10,67 @@ logger = logging.getLogger()
 @dataclass
 class GaussianMixtureModel:
 
-    num_dims: int
-    num_mixes: int
-    learning_rate: float = 0.001
+    def __init__(self, num_dims: int, num_mixes: int, learning_rate: float):
+        super().__init__()
 
-    def __post_init__(self):
-        self.cgmm = ConditionalGaussianMixtureModule(self.num_dims, self.num_mixes)
+        self.num_mixes = num_mixes
+        self.learning_rate = learning_rate
+
+        self.cgmm = ConditionalGaussianMixtureModule(num_dims, self.num_mixes)
         self.optimizer = optim.Adam(self.cgmm.parameters(), lr=self.learning_rate, weight_decay=0.0)
 
     def train(self, X, y):
 
-        distrs = self.cgmm(X)
-        # TODO(jalex): The line below is incorrect
-        reshaped = torch.reshape(distrs, (-1, 3, self.num_mixes))
-        reshaped = torch.transpose(torch.transpose(reshaped, 0, 1), 1, 2)
-        us, stds, weights = reshaped
-        nlls = torch.log(stds) + torch.square((y - us) / stds) / 2
-        weighted_nll = torch.sum(nlls * weights) / torch.sum(weights)
+        distrs = self._get_distrs(X)
+        reshaped = torch.transpose(distrs, 0, 2)
+        us, vars, weights = reshaped
+        nlls = (torch.log(vars) + torch.square((y - us) / vars)) / 2
+        # weighted_nll = torch.mean(torch.sum(nlls * weights, axis=0) / torch.sum(weights, axis=0))
+        # weighted_nll = torch.mean(nlls)
+        weighted_nll = torch.mean(torch.min(nlls, axis=0)[0])
 
         self.optimizer.zero_grad()
         weighted_nll.backward()
         self.optimizer.step()
 
         logger.info('Weighted nll %f', weighted_nll)
+        return weighted_nll
+
+    def evaluate(self, X):
+
+        with torch.no_grad():
+            return self._get_distrs(X)
+
+    def _get_distrs(self, X):
+        distrs = self.cgmm(X)
+        reshaped = torch.reshape(distrs, (-1, self.num_mixes, 3))
+        return reshaped
 
 
-@dataclass
 class ConditionalGaussianMixtureModule(nn.Module):
 
-    num_dims: int
-    num_mixes: int
+    def __init__(self, num_dims: int, num_mixes: int):
+        super().__init__()
+
+        self.num_dims = num_dims
+        self.num_mixes = num_mixes
+        h_sizes = [50, 30]
+
+        self.output_layers = nn.Sequential(
+            # TODO(jalex): this is messing up when all 0s. Create custom class to deal with this
+            # nn.BatchNorm1d(self.n_distr_dims + self.num_dims),
+            nn.Linear(self.n_distr_dims + self.num_dims, h_sizes[0]),
+            nn.LeakyReLU(0.01),
+            nn.BatchNorm1d(h_sizes[0]),
+            nn.Linear(h_sizes[0], h_sizes[1]),
+            nn.LeakyReLU(0.01),
+            nn.BatchNorm1d(h_sizes[1]),
+            nn.Linear(h_sizes[1], 3),  # u, var, weight
+        )
 
     @property
     def n_distr_dims(self):
         return 3 * self.num_mixes
-
-    def __post_init__(self):
-
-        self.output_layers = nn.ModuleList([
-            nn.BatchNorm1d(self.n_distr_dims + self.num_dims),
-            nn.Linear(self.num_dims, 50),
-            nn.LeakyReLU(0.01),
-            nn.BatchNorm1d(50),
-            nn.Linear(50, 3),  # u, std, weight
-        ])
 
     def forward(self, x):
         distrs = torch.zeros(len(x), self.n_distr_dims)
@@ -61,10 +78,10 @@ class ConditionalGaussianMixtureModule(nn.Module):
         for i in range(self.num_mixes):
             inp = torch.cat([distrs, x], axis=1)
             distr = self.output_layers(inp)
-            # TODO(jalex): Figure out best way of getting non neg val here
-            distr[1] = torch.abs(distr[1])
-            distr[2] = torch.sigmoid(distr[2])
-            distrs[i * 3: (i + 1) * 3] = distr
+            distrs[:, i * 3] = distr[:, 0]
+            # TODO(jalex): Figure out best way of getting non neg val here for var
+            distrs[:, i * 3 + 1] = torch.sqrt(torch.exp(distr[:, 1]))
+            distrs[:, i * 3 + 2] = torch.sigmoid(distr[:, 2])
 
         return distrs
 
